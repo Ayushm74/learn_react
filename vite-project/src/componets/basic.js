e0aa421ee3037dbad2bdc79a1fc24be6691559a8
Ayushm74 <Ayushmishra74992@gmail.com>
2026-03-22 01:07:26 +0530

    updated project

--- vite-project/src/componets/basic.js
+++ vite-project/src/componets/basic.js
@@ -7,3 +7,9 @@ funtion hello2()=>{
     return <div>Hello, World!   </div>
 }
 export default hello2;
+
+//Q1: Create a component that displays your name
+function Myname()=>{
+    return <div> my name </div>
+}
+epxort default Myname
