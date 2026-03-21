bcca54e845a16ea4b4c8dc79d2c721c0302de394
Ayushm74 <Ayushmishra74992@gmail.com>
2026-03-22 01:33:04 +0530

    updated project

--- vite-project/src/componets/basic.js
+++ vite-project/src/componets/basic.js
@@ -58,3 +58,14 @@ const counter()=>{
     }
 }
 exort default counter;
+
+import useState from react
+const counter()=>{
+    const[count, setcount] = useState(0);
+    return{
+        <div>
+        <h1>Count: {count}</h1>
+        <button onclick = {()=> setcount(count+1)}>incrsee</button>
+        </div>
+    }
+}
