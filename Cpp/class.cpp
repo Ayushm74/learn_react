75a445bdcb9316c2844954c090cf97720165ae01
Ayushm74 <Ayushmishra74992@gmail.com>
2026-03-26 16:42:27 +0530

    updated project

---
+++ Cpp/class.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+class Car{
+public:
+    virtual void start();
+    virtual void accelerate();
+    virtual void shiftgear();
+    virtual void breakk();
+    virtual void stop();
+};
+
