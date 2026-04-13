7c98e193d6164c34c148ad538edd7b54f9d99936
Ayushm74 <Ayushmishra74992@gmail.com>
2026-04-14 00:02:40 +0530

    updated project

--- Cpp/class_const.cpp
+++ Cpp/class_const.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
+using namespace std; 
 class students{
+public:
     int marks;
     string name;
 // default constructor
+    // students(){
+
+    // }
+public:
     students(){
+        cout<< "hello";
 
     }
+
 };
 
 int main(){
