cab03e6006537623cc6c4fe70399c7568506afc3
Ayushm74 <Ayushmishra74992@gmail.com>
2026-04-18 17:18:31 +0530

    updated project

--- vite-project/src/componets/basic.js
+++ vite-project/src/componets/basic.js
@@ -1,30 +1,11 @@
 import React from "react";
 
-function TodoApp() {
-
-  function callFun() {
-    alert("function called");
-  }
-
-  return (
-    <div>
-      <h1>Anil Sidhu Todos</h1>
-
-      <img
-        src="https://fastly.picsum.photos/id/237/250/250.jpg"
-        alt="Anil Sidhu"
-        className="photo"
-      />
-
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
-
-      <button onClick={callFun}>Click Me</button>
-    </div>
-  );
+function Button(){
+  const handleClick()=>{
+    alert("OMG");
+  };
+  return({
+    <button onClick={handleClick}>Click me<button>
+  });
+  export default Button;
 }
-
-export default TodoApp;
