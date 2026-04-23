a7444a48d93153d1f78614e987ab625baa0320f8
Ayushm74 <Ayushmishra74992@gmail.com>
2026-04-24 00:46:05 +0530

    updated project

--- Cpp/class_const.cpp
+++ Cpp/class_const.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 class Human{
-    public:
+    string Religion, color;
+
+    protected:
     string name;
     int age,weight;
 };
 
-class Student:protected Human{ 
+class Student:private Human{ 
+    private:
     int roll_no, fees;
+
+    public:
+    Student(string name, int age, int weight,int roll_number){
+        this->name = name;
+        this->roll_no = roll_no;
+        this->weight = weight;
+        this->age = age;
+    }
 };
 
 int main(){
