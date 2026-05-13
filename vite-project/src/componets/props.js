0b3bd102b98d9963b0a49392aaa061883a1f604f
Ayushm74 <Ayushmishra74992@gmail.com>
2026-05-14 00:16:13 +0530

    added cors

---
+++ vite-project/src/componets/props.js
@@ -0,0 +1,10 @@
+function UserCard(){
+    return(
+        <div style={{
+            border: "1px solid black",
+            padding: "10px",
+      margin: "10px",
+      width: "200px"
+        }}
+    )
+}
